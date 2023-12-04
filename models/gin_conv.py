371cef44eab95a0697172cfbdb1fecdf8c0793ca
Zhixing <zhixing.zhang@rutgers.edu>
2023-12-03 21:16:41 -0500

    d

--- models/gin_conv.py
+++ models/gin_conv.py
@@ -1,10 +1,10 @@
-from torch_geometric.nn.conv import GINEConv, GINConv, GATConv, GCNConv
+from torch_geometric.nn.conv import GINConv
 from typing import Callable, Union
 from torch import Tensor
 from torch_geometric.typing import OptPairTensor, Adj, OptTensor, Size
 
 
-class MyGINConv(GINEConv):
+class MyGINConv(GINConv):
     def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj, edge_weight,
                 size: Size = None) -> Tensor:
         """"""
